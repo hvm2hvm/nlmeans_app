93641df65c8d60e5f13a3631d79d1b9cb57d9c1c
Voicu Hodrea <hvm2hvm@gmail.com>
2017-05-13 17:19:33 +0300

    initial commit

---
+++ main.cpp
@@ -0,0 +1,7 @@
+#include "project.h"
+
+int main() {
+    cv::Mat m = cv::imread("/home/voicu/work/017_facultate/IP/Images/lab10/balloons_Gauss.bmp");
+
+    return 0;
+}
---
+++ project.h
@@ -0,0 +1,9 @@
+#ifndef PROJECT_H
+#define PROJECT_H
+
+#include <stdio.h>
+#include <opencv/cv.h>
+#include <opencv/highgui.h>
+#include <opencv/cvaux.h>
+
+#endif//PROJECT_H
